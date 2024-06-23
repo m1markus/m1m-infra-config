b25e845a8aa9b6b95474b3d352ffec8576fd1b9e
mmm <mmm7@bluewin.ch>
2024-06-23 12:19:00 +0200

    Add new class ChangeNotifier
    Emit the complete from the new module.

--- src/main/java/ch/m1m/infra/ConfigItemResource.java
+++ src/main/java/ch/m1m/infra/ConfigItemResource.java
@@ -6,8 +6,10 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.RestQuery;
+import org.jboss.resteasy.reactive.RestResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,25 +26,46 @@ public class ConfigItemResource {
 
     private static AtomicInteger atomicInteger = new AtomicInteger(0);
 
-    @Inject
-    ConfigItemService configItemService;
+    @Inject ConfigItemService configItemService;
+    @Inject ChangeNotifier changeNotifier;
 
     @Path("x")
     @GET
-    public Uni<String> getAsync(@RestQuery Integer delayMillis) {
+    public Uni<RestResponse<Object>> getAsync(@RestQuery Integer delayMillis,
+                                @RestQuery String domain,
+                                @RestQuery String application) {
         Duration delayRequestForMillis = Duration.ofMillis(30_000);
         if (delayMillis != null) {
             delayRequestForMillis = Duration.ofMillis(delayMillis.longValue());
         }
-        log.info("GET /config/x called... delaySeconds={}", delayRequestForMillis);
+        if (domain == null) {
+            domain = "it-ch";
+        }
+        if (application == null) {
+            application = "batch";
+        }
+        log.info("GET /config/x called... delaySeconds={} domain={} application={}",
+                delayRequestForMillis, domain, application);
+        String registerKey = "%s/%s".formatted(domain.trim(), application.trim());
+
         return Uni.createFrom().emitter(em -> {
             log.info("do something with the emitter instance...");
-            em.complete("toto");
+            changeNotifier.register(em, registerKey);
         })
-                .onItem().transform(item -> item + "-item-" + atomicInteger.incrementAndGet())
-                .ifNoItem().after(Duration.ofSeconds(4)).fail()
-                .onItem().delayIt().by(delayRequestForMillis);
+                .ifNoItem().after(delayRequestForMillis).recoverWithItem("")
+                .onItem().transform(item -> {
+                    if ("".equals(item)) {
+                        item = "noContent";
+                    } else {
+                        item = item + "-item-" + atomicInteger.incrementAndGet();
+                    }
+                    return RestResponse.ResponseBuilder.ok(item, MediaType.TEXT_PLAIN_TYPE)
+                            //.status(Response.Status.NO_CONTENT)
+                            .status(Response.Status.OK)
+                            .build();
+                });
 
+        //.onItem().delayIt().by(delayRequestForMillis);
         //return Uni.createFrom().item("toto");
     }
 
